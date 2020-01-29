fe63f857148c25b8377a76ce70955739caef0ecd
Admin <Admin@DESKTOP-FK7B3T9>
2020-01-29 13:43:29 +0200

    hh

--- Project/Project/src/application/HomeController.java
+++ Project/Project/src/application/HomeController.java
@@ -962,6 +962,7 @@ if(event.getSource() == backmsg)
 							s.ItemControllers(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), ch);
 						}
 					}
+					
 					else if(user.getType()==null)
 					{
 						s.ItemControllers(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), ch);
