b760420fa53c53b821382bea43b986aaa51c68ea
Admin <Admin@DESKTOP-FK7B3T9>
2020-01-29 13:42:51 +0200

    h

--- Project/Project/src/application/HomeController.java
+++ Project/Project/src/application/HomeController.java
@@ -818,6 +818,7 @@ if(event.getSource() == backmsg)
 	backmsg.setVisible(false);
 }
 		if (event.getSource() == mybusket) {
+			BusketList.clear();
 			Checkbusket.setVisible(true);
 			catpan.setVisible(false);
 			pnlCat.setVisible(false);
@@ -931,7 +932,7 @@ if(event.getSource() == backmsg)
 		}
 		if (event.getSource() == btncat) {
 			// pnlcat.setStyle("-fx-background-color : #1620A1");
-
+			Messagespnl.setVisible(false);
 			CatControlpnl.toFront();
 			CatControlpnl.setVisible(true);
 			Signuppan.setVisible(false);
@@ -992,6 +993,7 @@ if(event.getSource() == backmsg)
 		if (event.getSource() == complaintbtn) {
 			CatControlpnl.setVisible(false);
 			Complaintpnl.setVisible(true);
+			Messagespnl.setVisible(false);
 
 			Signuppan.setVisible(false);
 			Loginpan.setVisible(false);
@@ -1117,6 +1119,7 @@ if(event.getSource() == backmsg)
 
 			CatControlpnl.setVisible(true);
 			Signuppan.setVisible(false);
+			Messagespnl.setVisible(false);
 
 			Complaintpnl.setVisible(false);
 			UserControlpnl.setVisible(false);
@@ -1152,6 +1155,7 @@ if(event.getSource() == backmsg)
 
 			CatControlpnl.setVisible(false);
 			Signuppan.setVisible(false);
+			Messagespnl.setVisible(false);
 
 			Complaintpnl.setVisible(false);
 			UserControlpnl.setVisible(false);
@@ -1161,6 +1165,15 @@ if(event.getSource() == backmsg)
 			Loginpan.setVisible(true);
 		}
 		if (event.getSource() == btnfee) {
+			Messagespnl.setVisible(false);
+if(user.getRank().equals("2"))
+{
+	removedorder.setVisible(true);
+}
+else
+{
+	removedorder.setVisible(false);
+}
 			Orderspnl.toFront();
 			CatControlpnl.setVisible(false);
 			Signuppan.setVisible(false);
@@ -1551,13 +1564,16 @@ user.setType(resultset.getString(12));
 						CatControlpnl.toFront();
 						CatControlpnl.setVisible(true);
 						pnlCat.setVisible(true);
+						Messagespnl.setVisible(false);
 						table.setVisible(true);
 						Signuppan.setVisible(false);
 						Loginpan.setVisible(false);
 						labname.setText(user.getUsername());
 //Buttons of catalog
-						if(user.getRank() == "2")
+						System.out.println(user.getRank());
+						if(user.getRank().equals("2"))
 						{
+							System.out.println("a");
 							additem.setVisible(true);
 							delitem.setVisible(true);
 							edititem.setVisible(true);
@@ -1682,6 +1698,7 @@ user.setType(resultset.getString(12));
 		if (event.getSource() == btnuser) {
 
 			UserControlpnl.toFront();
+			Messagespnl.setVisible(false);
 
 			CatControlpnl.setVisible(false);
 			Signuppan.setVisible(false);
